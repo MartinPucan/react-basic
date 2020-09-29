4913a375002b182e17e73a867c4c38bafcbb56c7
MartinPucan <pucan.m@seznam.cz>
2020-09-29 20:22:27 +0200

    learn state && props

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const players = [
 	{
 		name: "Josh",
-		score: 100
+		score: 100,
+		id: 1
 	},
 	{
 		name: "Mike",
-		score: 20
+		score: 20,
+		id: 2
 	},
 	{
 		name: "Harvey",
-		score: 50
+		score: 50,
+		id: 3
 	},
 	{
 		name: "Rachel",
-		score: 30
+		score: 30,
+		id: 4
 	},
 	{
 		name: "Donna",
-		score: 90
+		score: 90,
+		id: 5
 	}
 ];
 
@@ -26,7 +31,7 @@ const Header = (props) => {
 	return (
 		<header>
 			<h1>{ props.title }</h1>
-			<span>Player: {props.totalPlayers}</span>
+			<span>Player: { props.totalPlayers }</span>
 		</header>
 	);
 }
@@ -37,31 +42,45 @@ const Player = (props) => {
 			<span className="player-name">
 				{ props.name }
 			</span>
-			<Counter score={props.score} />
+			<Counter score={ props.score } />
 		</div>
 	);
 }
 
-const Counter = (props) => {
-	return (
-		<div className="counter">
-			<button className="counter-action decrement"> - </button>
-			<span className="counter-score">{ props.score }</span>
-			<button className="counter-action increment"> + </button>
-		</div>
-	);
+class Counter extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			score: 0
+		};
+	}
+
+	render() {
+		return (
+			<div className="counter">
+				<button className="counter-action decrement"> - </button>
+				<span className="counter-score">{ this.props.score }</span>
+				<button className="counter-action increment"> + </button>
+			</div>
+		);
+	}
 }
 
 const App = (props) => {
 	return (
 		<div className="scoreboard">
-			<Header title="ScoreBoard" totalPlayers={1}/>
+			<Header
+				title="ScoreBoard"
+				totalPlayers={ props.initialsPlayers.length }
+			/>
 
 			{/* Prints all Players */}
-			{props.initialsPlayers.map( player =>
+			{ props.initialsPlayers.map( player =>
 				<Player
-					name={player.name}
-					score={player.score}
+					name={ player.name }
+					score={ player.score }
+					key={ player.id.toString() }
 				/>
 			)}
 		</div>
