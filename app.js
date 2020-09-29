cd2f29f1da1844223d0ecc6e1a30f2d0b7b5706b
MartinPucan <pucan.m@seznam.cz>
2020-09-29 20:57:18 +0200

    create decrement && increment methods and use state

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ const players = [
 ];
 
 const Header = (props) => {
-	console.log(props);
 	return (
 		<header>
 			<h1>{ props.title }</h1>
@@ -42,26 +41,36 @@ const Player = (props) => {
 			<span className="player-name">
 				{ props.name }
 			</span>
-			<Counter score={ props.score } />
+			<Counter />
 		</div>
 	);
 }
 
 class Counter extends React.Component {
 
-	constructor() {
-		super();
-		this.state = {
-			score: 0
-		};
+	state = {
+		score: 0
+	};
+
+	incrementScore = () => {
+		this.setState({
+			score: this.state.score + 1
+		});
+	}
+
+	decrementScore = () => {
+		this.setState({
+			score: this.state.score - 1
+		});
 	}
 
 	render() {
 		return (
 			<div className="counter">
-				<button className="counter-action decrement"> - </button>
-				<span className="counter-score">{ this.props.score }</span>
-				<button className="counter-action increment"> + </button>
+				<button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+				<span className="counter-score">{ this.state.score }</span>
+				<button className="counter-action increment"
+						onClick={this.incrementScore}> + </button>
 			</div>
 		);
 	}
@@ -79,7 +88,6 @@ const App = (props) => {
 			{ props.initialsPlayers.map( player =>
 				<Player
 					name={ player.name }
-					score={ player.score }
 					key={ player.id.toString() }
 				/>
 			)}
