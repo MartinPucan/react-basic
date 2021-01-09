e078e888c799aacfd4a3f9eb7c93cdf12290c902
MartinPucan <pucan.m@seznam.cz>
2021-01-10 00:13:52 +0100

    need remove project

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 
 import SearchForm from './SearchForm';
 import GifList from './GifList';
+import Nav from './Nav.js';
 
 function App() {
 
   return (
     <>
+			<Nav />
       <section className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+
+	return(
+		<section>
+			<nav className="nav">
+
+			</nav>
+		</section>
+	)
+}
+
+export default Nav;
